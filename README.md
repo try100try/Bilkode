8484cdaf15dc9610ed30626b2114ee6464e3768e
Gneemstar <47781206+try100try@users.noreply.github.com>
2020-03-06 23:02:58 +0100

    Tilføjelse af stepper motor
    Små ændringer lavet i todo listen, samt har jeg tilføjet et stepper motor library

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 #TODO
 1. Opsætning af afstandsmåler (done)
 2. Opsætning af LCD skærm samt LED lys til måling af afstand (done)
-3. tilføjelse af lyd til når bilen standser. 
-4. Stepper Motor til standsning af bilen.
+3. tilføjelse af lyd til når bilen standser. (god idé og nem at tilføje, dog er der kun 50% chance for den kommer ind)
+4. Stepper Motor til standsning af bilen. (done)
 5. Klargøring af stepper motor til bilen. (done)
 6. Montering af måler,steppermotor,display på bilen.
 7. Test, evt tweak af kode samt produkt.
-8. Skriv en proper todo liste.
+8. Færdiggør en dejlig todo liste.
--- bilkode.ino
+++ bilkode.ino
@@ -1,6 +1,6 @@
-//Hvis du har nogen som helst spørgsmål til min kode spørger du mig bare i timen og jeg vil med glæde svare på dine spørgsmål.
+//Hvis du har nogen som helst spørgsmål til min kode spørger du mig bare i timen og jeg vil med glæde svare.
 
-//Pins til afstandsmåleren, hvor 11 er den der modtager echo mens 12 er den der udskyder lydbølgen
+//Pins til afstandsmåleren, hvor 11 er den der modtager lydbølgen mens 12 er den der udskyder lydbølgen
 #define trigPin 12
 #define echoPin 11
 //Duration er mængden af tid der gik før at den modtog lydbølgen, distance er værdien som bliver udregned med lydbølgen.
@@ -15,9 +15,20 @@ int blue = A0;
 #include <LiquidCrystal.h>
 //Her kalder jeg dens pins (information)
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+//Stepper motor Library
+#include <Stepper.h>
+//rotationStepper er hvor langt at motoren skal bevæge sig hver den den bevæger sig.
+const int rotationStepper = 250; 
+//hastighedStepper er hastigheden at stepper motoren bevæger sig i min/max range: 0-17rpm.
+const int hastighedStepper = 15;
+//Deklaration af stepper motoren med sine pins.
+Stepper myStepper(rotationStepper, 8, 10, 9, 13);
+
 
 //Når man kører programmet for første gang er det denne function der bliver kaldt
 void setup() {
+  //Her sætter jeg bare stepper motorens hastighed til dens variabel hastighedStepper
+    myStepper.setSpeed(hastighedStepper);
 //Her bliver der bare deklareret output og input på de two lydbølge banditter.
 pinMode(trigPin,OUTPUT);
 pinMode(echoPin,INPUT);
@@ -48,19 +59,23 @@ distance = duration*0.034/2;
 Serial.print("Distance: ");
 Serial.println(distance);
 //Her skriver jeg bare at mine LED lys skal tænde baseret på distance, så hvis man kommer tæt på en væg bliver lysene tændt.
-delay(100);
+
 //Distance er i centimeter btw,jo lavere distance er, jo flere LED lys bliver tændt og der er en rangliste af hvor slemme de er, blå er altid tændt, grøn er et stykke væk, gul er tæt på, rød er farligt tæt på.
-if (distance<=60){
+if (distance<=100){
 digitalWrite(green,HIGH);
 } else {digitalWrite(green,LOW);}
 
-if (distance<=40){
+if (distance<=60){
 digitalWrite(yellow,HIGH);
 } else {digitalWrite(yellow,LOW);}
 
 if (distance<=20){
 digitalWrite(red,HIGH);
-} else {digitalWrite(red,LOW);}
+//Når den røde lampe er tændt, bliver funktionen brems kaldt, som er en funktion der tænder stepper motoren så den bremser bilen.
+brems();
+} else {
+  digitalWrite(red,LOW);
+  digitalWrite(blue,HIGH);}
 
 //setCursor er hvor der skal skrives noget text, x,y værdier. Når y=0 er den øverst, når y=1 er den nederst.
     lcd.setCursor(0, 1);
@@ -69,7 +84,18 @@ digitalWrite(red,HIGH);
 //Det er ligesom at skrive background() i draw i javascript, den renser bare canvas så der ikke står wack tal i baggrunden og roder, så man kan rent faktisk læse teksten der bliver givet nu).
 //LCD'et der bliver brugt i programmet er 16 blokke lange og 2 høje, men hvis du bruger en anden LCD skærm skal du nok ændre mængden af mellemrum i min lcd.print("mellemrum"x16) linje.
 lcd.print("                ");
-//Delayen gør det hele bare lidt bedre og mere smooth, da hurtig tekst er svær tekst :()
+//Delayen gør det hele bare lidt bedre både grafisk, og så nogle af delene bruger lidt mindre strøm så den kan køre ordentligt på eklusivt 9volts batterier.
 delay(250);
   
+}
+
+void brems(){
+Serial.println("Jeg bremser nu");
+digitalWrite(blue,LOW);
+
+myStepper.step(rotationStepper);
+delay(1500);
+myStepper.step(-rotationStepper);
+delay(6000);
+
 }
